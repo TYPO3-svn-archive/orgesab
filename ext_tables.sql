5a730b3f71fc6bb09857026589d9277af1a5c9aa
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-03-29 14:24:22 +0000

    0.0.1 - DB
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/orgesab/trunk@73938 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.sql
+++ ext_tables.sql
@@ -77,6 +77,7 @@ CREATE TABLE tx_orgesab_cat (
   cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
   deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
   title tinytext,
+  uid_parent int(11) DEFAULT '0' NOT NULL,
   hidden tinyint(4) DEFAULT '0' NOT NULL,
   
   PRIMARY KEY (uid),
