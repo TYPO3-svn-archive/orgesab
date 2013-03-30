07b21c6f043536410225afe42326c58a3ceaa23b
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-03-30 01:28:02 +0000

    0.0.1 - TypoScript
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/orgesab/trunk@74016 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "orgesab".
  *
- * Auto generated 28-03-2013 02:48
+ * Auto generated 30-03-2013 02:27
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'browser' => '',
-                        'cps_tcatree' => '',
+			'cps_tcatree' => '',
 			'css_styled_content' => '',
 			'linkhandler' => '',
 		),
@@ -46,7 +46,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:49:{s:9:"ChangeLog";s:4:"dfcd";s:16:"ext_autoload.php";s:4:"00f6";s:21:"ext_conf_template.txt";s:4:"c6b7";s:12:"ext_icon.gif";s:4:"ec42";s:17:"ext_localconf.php";s:4:"ccbc";s:14:"ext_tables.php";s:4:"ab9b";s:14:"ext_tables.sql";s:4:"4390";s:16:"locallang_db.xml";s:4:"6286";s:7:"tca.php";s:4:"50c5";s:8:"TODO.txt";s:4:"d41d";s:14:"doc/manual.pdf";s:4:"a62b";s:14:"doc/manual.sxw";s:4:"0b4e";s:17:"ext_icon/esab.gif";s:4:"e755";s:32:"lib/class.tx_orgesab_convert.php";s:4:"06b2";s:35:"lib/class.tx_orgesab_extmanager.php";s:4:"00b4";s:28:"lib/class.tx_orgesab_get.php";s:4:"4403";s:31:"lib/class.tx_orgesab_update.php";s:4:"e16d";s:17:"lib/locallang.xml";s:4:"8f15";s:45:"lib/scheduler/class.tx_orgesab_importtask.php";s:4:"7373";s:69:"lib/scheduler/class.tx_orgesab_importtask_additionalfieldprovider.php";s:4:"4e10";s:44:"lib/scheduler/class.tx_orgesab_quotatask.php";s:4:"0889";s:68:"lib/scheduler/class.tx_orgesab_quotatask_additionalfieldprovider.php";s:4:"77e5";s:43:"lib/scheduler/class.tx_orgesab_testtask.php";s:4:"f73c";s:67:"lib/scheduler/class.tx_orgesab_testtask_additionalfieldprovider.php";s:4:"520c";s:27:"lib/scheduler/locallang.xml";s:4:"05a8";s:20:"res/realurl_conf.php";s:4:"5203";s:25:"static/base/constants.txt";s:4:"80d1";s:21:"static/base/setup.txt";s:4:"e258";s:35:"static/base/typo3/4.6/constants.txt";s:4:"d41d";s:31:"static/base/typo3/4.6/setup.txt";s:4:"0fbf";s:33:"static/calendar/201/constants.txt";s:4:"d41d";s:29:"static/calendar/201/setup.txt";s:4:"fc1b";s:41:"static/calendar/201/expired/constants.txt";s:4:"d41d";s:37:"static/calendar/201/expired/setup.txt";s:4:"2ef4";s:33:"static/calendar/211/constants.txt";s:4:"d41d";s:29:"static/calendar/211/setup.txt";s:4:"115d";s:31:"static/esab/46476/constants.txt";s:4:"d41d";s:27:"static/esab/46476/setup.txt";s:4:"6e06";s:20:"tsConfig/de/page.txt";s:4:"90c1";s:25:"tsConfig/default/page.txt";s:4:"90c1";s:19:"xml_test/detail.php";s:4:"db6c";s:23:"xml_test/detaildata.php";s:4:"1858";s:17:"xml_test/list.php";s:4:"54ac";s:21:"xml_test/listdata.php";s:4:"ec7d";s:17:"xml_test/test.php";s:4:"ac55";s:22:"xml_test/css/basic.css";s:4:"db0f";s:21:"xml_test/css/typo.css";s:4:"e0e7";s:29:"xml_test/xml-daten/import.xml";s:4:"eda4";s:34:"xml_test/xml-struktur/struktur.xml";s:4:"d634";}',
+	'_md5_values_when_last_written' => 'a:47:{s:9:"ChangeLog";s:4:"dfcd";s:16:"ext_autoload.php";s:4:"d76c";s:21:"ext_conf_template.txt";s:4:"d492";s:12:"ext_icon.gif";s:4:"ec42";s:17:"ext_localconf.php";s:4:"0a04";s:14:"ext_tables.php";s:4:"dcfa";s:14:"ext_tables.sql";s:4:"2761";s:16:"locallang_db.xml";s:4:"ca43";s:7:"tca.php";s:4:"afae";s:8:"TODO.txt";s:4:"d41d";s:14:"doc/manual.pdf";s:4:"a62b";s:14:"doc/manual.sxw";s:4:"0b4e";s:17:"ext_icon/esab.gif";s:4:"e755";s:32:"lib/class.tx_orgesab_convert.php";s:4:"5022";s:35:"lib/class.tx_orgesab_extmanager.php";s:4:"00b4";s:28:"lib/class.tx_orgesab_get.php";s:4:"7546";s:31:"lib/class.tx_orgesab_update.php";s:4:"8e6a";s:17:"lib/locallang.xml";s:4:"8f15";s:45:"lib/scheduler/class.tx_orgesab_importtask.php";s:4:"3eb8";s:69:"lib/scheduler/class.tx_orgesab_importtask_additionalfieldprovider.php";s:4:"e963";s:43:"lib/scheduler/class.tx_orgesab_testtask.php";s:4:"d875";s:67:"lib/scheduler/class.tx_orgesab_testtask_additionalfieldprovider.php";s:4:"dc15";s:27:"lib/scheduler/locallang.xml";s:4:"05a8";s:20:"res/realurl_conf.php";s:4:"5203";s:25:"static/base/constants.txt";s:4:"31b1";s:21:"static/base/setup.txt";s:4:"367e";s:35:"static/base/typo3/4.6/constants.txt";s:4:"d41d";s:31:"static/base/typo3/4.6/setup.txt";s:4:"0fbf";s:33:"static/calendar/201/constants.txt";s:4:"d41d";s:29:"static/calendar/201/setup.txt";s:4:"fc1b";s:41:"static/calendar/201/expired/constants.txt";s:4:"d41d";s:37:"static/calendar/201/expired/setup.txt";s:4:"2ef4";s:33:"static/calendar/211/constants.txt";s:4:"d41d";s:29:"static/calendar/211/setup.txt";s:4:"115d";s:32:"static/views/46476/constants.txt";s:4:"68b3";s:28:"static/views/46476/setup.txt";s:4:"31f2";s:20:"tsConfig/de/page.txt";s:4:"90c1";s:25:"tsConfig/default/page.txt";s:4:"90c1";s:19:"xml_test/detail.php";s:4:"db6c";s:23:"xml_test/detaildata.php";s:4:"1858";s:17:"xml_test/list.php";s:4:"54ac";s:21:"xml_test/listdata.php";s:4:"ec7d";s:17:"xml_test/test.php";s:4:"ac55";s:22:"xml_test/css/basic.css";s:4:"db0f";s:21:"xml_test/css/typo.css";s:4:"e0e7";s:29:"xml_test/xml-daten/import.xml";s:4:"eda4";s:34:"xml_test/xml-struktur/struktur.xml";s:4:"d634";}',
 );
 
 ?>
