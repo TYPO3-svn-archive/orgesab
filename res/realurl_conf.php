c562fa7c9f7982332c552ca16f235e88351b8b0a
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-05-19 13:53:38 +0000

    0.0.9 +++ RealURL update
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/orgesab/trunk@85335 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/realurl_conf.php
+++ res/realurl_conf.php
@@ -3,7 +3,7 @@
   //////////////////////////////////
   //
   // TEMPLATE
-  
+
   // Template file for
   // typo3conf/realurl_conf.php
 
@@ -57,17 +57,17 @@ $TYPO3_CONF_VARS['EXTCONF']['realurl'] = array
 
 $TYPO3_CONF_VARS['EXTCONF']['realurl']['_DEFAULT']['postVarSets'] = array
 (
-  '_DEFAULT' => array 
+  '_DEFAULT' => array
   (
-    // news: organiser_news
-    'news' => array 
+    // offer: organiser_news
+    'offer' => array
     (
       array
       (
-        'GETvar' => 'tx_browser_pi1[showUid]',
+        'GETvar' => 'tx_browser_pi1[orgesabUid]',
         'lookUpTable' => array
         (
-          'table'               => 'tx_organiser_news',
+          'table'               => 'tx_orgesab',
           'id_field'            => 'uid',
           'alias_field'         => 'title',
           'addWhereClause'      => ' AND NOT deleted AND NOT hidden',
@@ -80,27 +80,27 @@ $TYPO3_CONF_VARS['EXTCONF']['realurl']['_DEFAULT']['postVarSets'] = array
         )
       ),
     ),
-    'browse' => array 
+    'browse' => array
     (
-      array 
+      array
       (
         'GETvar' => 'tx_browser_pi1[azTab]',
       ),
-      array 
+      array
       (
         'GETvar' => 'tx_browser_pi1[pointer]',
       ),
     ),
-    'sort' => array 
+    'sort' => array
     (
-      array 
+      array
       (
         'GETvar' => 'tx_browser_pi1[sort]',
       ),
     ),
-    'suche' => array 
+    'suche' => array
     (
-      array 
+      array
       (
         'GETvar' => 'tx_browser_pi1[sword]',
       ),
